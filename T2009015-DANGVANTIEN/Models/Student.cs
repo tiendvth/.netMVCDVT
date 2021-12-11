ee40323828eff84f40ac4836028ecdd5c26aaafa
unknown <you@example.com>
2021-12-11 10:40:02 +0700

    tien

---
+++ T2009015-DANGVANTIEN/Models/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace T2009015_DANGVANTIEN.Models
+{
+    public class Student
+    {
+        public string Masv { get; set; }
+        public string Phat { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
